1a6541e93f4ceba88639bd5b7ee2a17870ba62d0
changcc5 <changcc5@cs.ucla.edu>
2015-07-10 15:29:18 -0700

    data

--- Shopify/Model/ShopifyModel.php
+++ Shopify/Model/ShopifyModel.php
@@ -1,4 +1,23 @@
 <?php
-phpinfo();
-echo "Hello";
+$servername = "localhost";
+$username = "root";
+$password = "Dell143$";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=Shopify", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT * FROM Inventory ORDER BY item_no;";
+    // use exec() because no results are returned
+	foreach ($conn->query($sql) as $row) {
+        print $row['nam'] . "\t";
+        print $row['item_no'] . "\n";
+    }
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
 ?>
