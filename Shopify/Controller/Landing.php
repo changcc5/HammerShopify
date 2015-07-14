a9a373d01d640391d4de3c1165ec0aa03c3069ea
changcc5 <changcc5@cs.ucla.edu>
2015-07-14 16:19:06 -0700

    modified

--- Shopify/Controller/Landing.php
+++ Shopify/Controller/Landing.php
@@ -1,5 +1,8 @@
 <?php
-include '../Model/ShopifyModel.php';
+require_once '../Model/ShopifyModel.php';
+require_once '../View/ShopifyView.php';
 
-echo "git committed";
+$page = new ShopifyView();
+
+echo $page->getPage();
 ?>
--- Shopify/View/ShopifyView.php
+++ Shopify/View/ShopifyView.php
@@ -1,9 +1,51 @@
 <?php
 
 class ShopifyView {
-function __constructor() {
-
-}
+	function __constructor() {
+	
+	}
+	
+	public function getPage($page='home', $data='null') {
+		$page = '<!DOCTYPE html>
+		<html>
+		<head>
+		' . $this->getHead($page, $data) . '
+		</head>
+		<body>
+		' . $this->getBody($page, $data) .'
+		</body>
+		</html>
+		';
+		return $page;
+	}
+	public function getHead($page, $data) {
+		
+	}
+	
+	public function getBody($page, $data) {
+		switch ($page) {
+			case 'home':
+				$body = '
+				<ul>
+					<li><a href="../Controller/Landing.php?page=inventory">Inventory</a></li>
+					<li><a href="../Controller/Landing.php?page=report">Reports</a></li>
+				</ul>
+				';
+				break;
+			case 'inventory':
+				$body = '
+				<table>
+				
+				</table>
+				';
+				break;
+			default:
+				
+				break;
+		}
+		
+		return $body;
+	}
 }
 
 ?>
