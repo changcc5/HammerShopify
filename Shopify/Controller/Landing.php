a95e8a5045d00981aa1c154a821db0e2b0d0a116
changcc5 <changcc5@cs.ucla.edu>
2015-07-15 16:52:31 -0700

    mo;del

--- Shopify/Controller/Landing.php
+++ Shopify/Controller/Landing.php
@@ -3,18 +3,18 @@ require_once '../Model/ShopifyModel.php';
 require_once '../View/ShopifyView.php';
 
 $page = new ShopifyView();
+$model = new ShopifyModel();
 
-if(!empty($_GET['page'])) {
-	switch (strip_tags($_GET['page'])) {
-		case 'inventory':
-			$inventory = ViewInventory();
-			echo $page->getPage('inventory',$inventory);
-			break;
-		
-		default:
-			
-			break;
-	}
-}
+// if(!empty($_GET['page'])) {
+	// switch (strip_tags($_GET['page'])) {
+		// case 'inventory':
+			// echo $page->getPage('inventory',$inventory);
+			// break;
+// 		
+		// default:
+// 			
+			// break;
+	// }
+// }
 echo $page->getPage();
 ?>
--- Shopify/Model/ShopifyModel.php
+++ Shopify/Model/ShopifyModel.php
@@ -3,8 +3,13 @@ $servername = "localhost";
 $username = "root";
 $password = "Dell143$";
 
+try {
 $con = new PDO("mysql:host=$servername;dbname=Shopify", $username, $password); 
-
+$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+	echo $e->getMessage();
+}
 function ViewInventory() {
 	try{
 			$sql = "SELECT * FROM Inventory ORDER BY item_no;";
@@ -17,11 +22,11 @@ function ViewInventory() {
 	    }
 }
 
-class ShopifyModel {
-	public $conn;
-	function __construct() {
-		try {
-		    $this->conn = new PDO("mysql:host=localhost;dbname=Shopify", "root", "Dell143$");
+// class ShopifyModel {
+	// public $conn;
+	// function __construct() {
+		// try {
+		    // $this->conn = new PDO("mysql:host=localhost;dbname=Shopify", "root", "Dell143$");
 		    // // set the PDO error mode to exception
 		    // $this->$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		    // $sql = "SELECT * FROM Inventory ORDER BY item_no;";
@@ -30,41 +35,41 @@ class ShopifyModel {
 		        // print $row['nam'] . "\t";
 		        // print $row['item_no'] . "\n";
 		    // }
-		    }
-		catch(PDOException $e)
-		    {
-		    // echo $sql . "<br>" . $e->getMessage();
-		    }
-		
-		$conn = null;
-	}
-	
-	public function UpdateInventory($item_no, $fields, $value) {
-		foreach ($fields as $key => $field) {
-			$sql = $conn->prepare('UPDATE Inventory Set ? = ? WHERE item_no = ?;');
-			
-			// $sql->execute(array($field))
-		}
-	}
-	public function NewInventory($data) {
-		
-	}
-	public function DeleteInventory($item) {
-		
-	}
-	public function ViewInventory() {
-		try{
-			$sql = "SELECT * FROM Inventory ORDER BY item_no;";
-			$result = $this->conn->query($sql);
-			return $result;
-		}
-		catch(PDOException $e)
-	    {
-	    	echo $sql . "<br>" . $e->getMessage();
-	    }
-	}
-	public function RunReport($fields) {
-		
-	}
-}
+		    // }
+		// catch(PDOException $e)
+		    // {
+		    // // echo $sql . "<br>" . $e->getMessage();
+		    // }
+// 		
+		// $conn = null;
+	// }
+// 	
+	// public function UpdateInventory($item_no, $fields, $value) {
+		// foreach ($fields as $key => $field) {
+			// $sql = $conn->prepare('UPDATE Inventory Set ? = ? WHERE item_no = ?;');
+// 			
+			// // $sql->execute(array($field))
+		// }
+	// }
+	// public function NewInventory($data) {
+// 		
+	// }
+	// public function DeleteInventory($item) {
+// 		
+	// }
+	// public function ViewInventory() {
+		// try{
+			// $sql = "SELECT * FROM Inventory ORDER BY item_no;";
+			// $result = $this->conn->query($sql);
+			// return $result;
+		// }
+		// catch(PDOException $e)
+	    // {
+	    	// echo $sql . "<br>" . $e->getMessage();
+	    // }
+	// }
+	// public function RunReport($fields) {
+// 		
+	// }
+// }
 ?>
